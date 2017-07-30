e4e0ea38938fb1b79de6475a01dcbcab872f9ae5
@shiregator <shiregator@users.noreply.github.com>
2017-07-29 23:30:01 -0500

    Initial Commit.

--- CorruptedFileFixer/Form1.Designer.cs
+++ CorruptedFileFixer/Form1.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.folderNameLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.log = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.mainProgress = new System.Windows.Forms.ProgressBar();
+            this.simulateCB = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Location = new System.Drawing.Point(12, 12);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(75, 23);
+            this.selectFolderButton.TabIndex = 0;
+            this.selectFolderButton.Text = "Select folder";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectFolderButton_Click);
+            // 
+            // folderNameLabel
+            // 
+            this.folderNameLabel.AutoSize = true;
+            this.folderNameLabel.Location = new System.Drawing.Point(93, 17);
+            this.folderNameLabel.Name = "folderNameLabel";
+            this.folderNameLabel.Size = new System.Drawing.Size(0, 13);
+            this.folderNameLabel.TabIndex = 1;
+            // 
+            // startButton
+            // 
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(12, 41);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(12, 112);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            this.log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.log.Size = new System.Drawing.Size(530, 257);
+            this.log.TabIndex = 3;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(12, 96);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(35, 13);
+            this.statusLabel.TabIndex = 4;
+            this.statusLabel.Text = "label2";
+            // 
+            // mainProgress
+            // 
+            this.mainProgress.Location = new System.Drawing.Point(12, 70);
+            this.mainProgress.Name = "mainProgress";
+            this.mainProgress.Size = new System.Drawing.Size(530, 23);
+            this.mainProgress.TabIndex = 5;
+            // 
+            // simulateCB
+            // 
+            this.simulateCB.AutoSize = true;
+            this.simulateCB.Location = new System.Drawing.Point(106, 45);
+            this.simulateCB.Name = "simulateCB";
+            this.simulateCB.Size = new System.Drawing.Size(88, 17);
+            this.simulateCB.TabIndex = 6;
+            this.simulateCB.Text = "Only find files";
+			this.simulateCB.Checked = true;
+            this.simulateCB.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(554, 381);
+            this.Controls.Add(this.simulateCB);
+            this.Controls.Add(this.mainProgress);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.folderNameLabel);
+            this.Controls.Add(this.selectFolderButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Corrupted files fixer";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.Label folderNameLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.ProgressBar mainProgress;
+        private System.Windows.Forms.CheckBox simulateCB;
     }
 }
 
--- CorruptedFileFixer/Form1.cs
+++ CorruptedFileFixer/Form1.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +13,133 @@ namespace CorruptedFileFixer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string selectedFolder;
+        private List<string> imagesToFix = new List<string>();
+
+        private void selectFolderButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = false;
+            statusLabel.Text = "";
+            mainProgress.Value = 0;
+            imagesToFix.Clear();
+            folderNameLabel.Text = "";
+            var dial = new FolderBrowserDialog();
+            if (dial.ShowDialog() == DialogResult.OK)
+            {
+                startButton.Enabled = !String.IsNullOrWhiteSpace(dial.SelectedPath);
+                folderNameLabel.Text = dial.SelectedPath;
+                selectedFolder = dial.SelectedPath;
+                statusLabel.Text = "Ready!";
+            }
+        }
+
+        private async void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Enabled = false;
+            selectFolderButton.Enabled = false;
+            statusLabel.Text = "Scanning folder for images..";
+            check = 0;
+            found = 0;
+            var filesCount = await ScanFolderForJPG(selectedFolder);
+            mainProgress.Maximum = filesCount;
+            statusLabel.Text = "Found " + filesCount + " corrupted images.. Fixing..";
+            log.Clear();
+            foreach(string img in imagesToFix)
+            {
+                if (simulateCB.Checked || await FixImage(img))
+                {
+                    log.Text += img + (!simulateCB.Checked ? " *fixed" : "") + Environment.NewLine;
+                } else
+                {
+                    log.Text += img + " *FAILED TO FIX" + Environment.NewLine;
+                }
+                mainProgress.Value++;
+            }
+            statusLabel.Text = "Done!";
+            selectFolderButton.Enabled = true;
+        }
+
+        private void SetStatusLabelText(string text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke((Action<string>)SetStatusLabelText, text);
+                return;
+            }
+            statusLabel.Text = text;
+        }
+
+        int check;
+        int found;
+
+        private async Task<int> ScanFolderForJPG(string path)
+        {
+            int res = 0;
+            try
+            {
+                foreach (string file in Directory.GetFiles(path))
+                {
+                    Application.DoEvents();
+                    if (Path.GetExtension(file).ToLower() == ".jpg")
+                    {
+                        check++;
+                        SetStatusLabelText("Scanning " + path + " .. Checked " + check + " files. Found " + found + " corrupted files..");
+                        using (FileStream str = File.Open(file, FileMode.Open, FileAccess.Read, FileShare.Delete | FileShare.ReadWrite))
+                        {
+                            if (str.Length < 100) //can image be less than 100 bytes?
+                                continue;
+                            using (BinaryReader read = new BinaryReader(str))
+                            {
+                                if (read.ReadUInt64() == 0x0A1A0A0D474E5089L) //checking png signature;
+                                {
+                                    imagesToFix.Add(file);
+                                    res++;
+                                    found++;
+                                }
+                            }
+                        }
+                    }
+                }
+                foreach (string dir in Directory.GetDirectories(path))
+                {
+                    res += await ScanFolderForJPG(dir);
+                }
+            } catch (Exception e)
+            {
+                log.Text += e.Message + Environment.NewLine;
+            }
+            return res;
+        }
+
+        private async Task<bool> FixImage(string image)
+        {
+            try
+            {
+                using (FileStream str = File.OpenRead(image))
+                {
+                    using (Image img = Image.FromStream(str))
+                    {
+                        if (img.RawFormat.Guid == ImageFormat.Png.Guid)
+                        {
+                            img.Save(image + "tmp", ImageFormat.Jpeg);
+                        }
+                    }
+                }
+                File.Delete(image);
+                File.Move(image + "tmp", image);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
